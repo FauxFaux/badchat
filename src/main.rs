0b93596791acbef32a3ddf7872e29051b5d3f0c0
Chris West (Faux) <git@goeswhere.com>
2018-10-17 21:43:51 +0100

    stdlib channel per client

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,36 @@
 #[macro_use]
-extern crate crossbeam_channel;
-#[macro_use]
 extern crate failure;
 
-use std::net;
 use std::io;
 use std::io::BufRead;
 use std::io::Write;
+use std::net;
+use std::sync;
 use std::thread;
 
 use failure::Error;
-use crossbeam_channel as channel;
 
 fn main() -> Result<(), Error> {
     let serv = net::TcpListener::bind("127.0.0.1:3730")?;
 
-    let (s, r) = channel::unbounded();
+    let live_clients = sync::Arc::new(sync::Mutex::new(Vec::new()));
 
     for client in serv.incoming() {
-        let mut client = client?;
-        let mut read = io::BufReader::new(&client);
-        let s = s.clone();
-        thread::spawn(move || {
-            loop {
-                let mut str = String::new();
-                read.read_line(&mut str).unwrap();
-                s.send(str);
+        let client = client?;
+        let mut client = io::BufReader::new(client);
+        let (s, r) = sync::mpsc::channel();
+        live_clients.lock().unwrap().push(s);
+        let live_clients = live_clients.clone();
+        thread::spawn(move || loop {
+            let mut buf = String::new();
+            client.read_line(&mut buf).unwrap();
+            let buf = buf.trim().to_string();
+            for client in live_clients.lock().unwrap().iter() {
+                client.send(buf.to_string()).unwrap();
             }
-        });
 
-        thread::spawn(move || {
-            while let Some(msg) = r.recv() {
-               write!(client, "{}", msg);
+            while let Ok(msg) = r.try_recv() {
+                writeln!(client.get_mut(), "{}", msg).unwrap();
             }
         });
     }
