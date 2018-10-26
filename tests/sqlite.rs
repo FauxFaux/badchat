617bf1dca00c1138d1045578ffc70d46b7ec28e9
Chris West (Faux) <git@goeswhere.com>
2018-10-26 20:32:27 +0100

    quick panic about sqlite unicode handling

---
+++ tests/sqlite.rs
@@ -0,0 +1,35 @@
+extern crate failure;
+extern crate rusqlite;
+
+use failure::Error;
+use rusqlite::Connection;
+
+/// quick panic about: If the first byte of the payload is 0x00, 0x01, or 0x02
+/// then that byte is ignored and the remaining bytes are UTF8, UTF16le, or UTF16be
+/// respectively. If the first byte is 0x03 or larger, then the entire string including
+/// the first byte is UTF8. An empty string consists of the header code 22 and no payload.
+#[test]
+fn round_trip() -> Result<(), Error> {
+    let conn = Connection::open_in_memory()?;
+    conn.execute(
+        "create table strings (val text not null)",
+        rusqlite::NO_PARAMS,
+    )?;
+    let mut trunc = conn.prepare("delete from strings")?;
+    let mut write = conn.prepare("insert into strings (val) values (?)")?;
+    let mut read = conn.prepare("select val from strings")?;
+    for c in 1..255 {
+        trunc.execute(rusqlite::NO_PARAMS)?;
+
+        let mut before = String::with_capacity(16);
+        let c_as_char = std::char::from_u32(c).expect("from_u32");
+        before.push(c_as_char);
+        before.push(c_as_char);
+        before.push(c_as_char);
+
+        write.execute(&[&before])?;
+        let after: String = read.query_row(rusqlite::NO_PARAMS, |row| row.get(0))?;
+        assert_eq!(before, after);
+    }
+    Ok(())
+}
