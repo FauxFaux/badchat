de8c5c34cdaeb10a5f786a9b15b8c1b3af365d78
Chris West (Faux) <git@goeswhere.com>
2018-11-12 22:10:25 +0000

    invent

--- src/ids.rs
+++ src/ids.rs
@@ -1,5 +1,28 @@
 use std::fmt;
 
+// Nick routing.
+//
+// A... `talker` can talk. It can talk in channels, talk to other talkers directly,
+// and is what people want to talk to.
+//
+// A connection is a tcp(/tls) socket.
+//
+// An account is a group of passwords, and some capabilities.
+//
+// We're enforcing that a connection has an account.
+// A connection should probably have at least one talker associated with it,
+// just so we can refer to it?
+//
+// A standard IRC client is represented as a single talker. We allow the account
+// to connect if they can authenticate, and resume as the talker. We should probably
+// disconnect people on timeout as normal, although maybe with a far smaller ping-out (30s?).
+//
+// Traditional services are a single connection/account, but multiple talkers, and some caps.
+//
+// Bridges are... a single connection/account, with multiple talkers, and no extra caps? Beyond
+// the "multiple talker" cap?
+
+
 // Concern: Almost identical structs here, could share a Target / Ident / ?
 // Concern: Is a nick different from an ident (nick!~user@host)? An unresolved ident?
 
