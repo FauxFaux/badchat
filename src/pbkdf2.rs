f845161a01e4f2dd369696f0d700ae5e5b7e8f30
Chris West (Faux) <git@goeswhere.com>
2024-03-09 12:23:09 +0000

    feat: bumps

--- src/pbkdf2.rs
+++ src/pbkdf2.rs
@@ -7,8 +7,15 @@ use pbkdf2::Pbkdf2;
 
 pub fn pbkdf2_simple(pass: &str) -> Result<String> {
     let salt = SaltString::generate(rand::thread_rng());
+    let mut params = pbkdf2::Params::default();
+    if cfg!(test) {
+        // default is unusably slow in test (debug configuration), >30 seconds (2024)
+        params.rounds = 1000;
+    } else {
+        assert!(params.rounds >= 600_000);
+    }
     let hash = Pbkdf2
-        .hash_password_simple(pass.as_bytes(), &salt)
+        .hash_password_customized(pass.as_bytes(), None, None, params, &salt)
         .map_err(|e| anyhow!("{:?}", e))?;
     Ok(hash.to_string())
 }
